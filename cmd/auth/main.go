b4006b6c86c98f2eabd5fc0ffcf31c4d502dc816
astronely <nikborowik200394@gmail.com>
2024-07-02 21:13:34 +0500

    add auth service with stubs

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -2,43 +2,13 @@ package main
 
 import (
 	"github.com/astronely/pastebin/internal/app"
-	"github.com/astronely/pastebin/pkg/logger"
-	"github.com/joho/godotenv"
-	"log/slog"
-	"os"
-	"regexp"
+	"log"
 )
 
-const projectDirName = "Pastebin" // change to relevant project name
-
-func init() {
-	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
-	currentWorkDirectory, _ := os.Getwd()
-	rootPath := projectName.Find([]byte(currentWorkDirectory))
-	if err := godotenv.Load(string(rootPath) + `/.env`); err != nil {
-		slog.Error("No .env file found")
-	}
-}
-
-func initLogger() *slog.Logger {
-	opts := logger.PrettyHandlerOptions{
-		SlogOpts: slog.HandlerOptions{
-			Level: slog.LevelDebug,
-		},
-	}
-	handler := logger.NewPrettyHandler(os.Stdout, opts)
-	log := slog.New(handler)
-	slog.SetDefault(log)
-	return log
-}
-
 func main() {
-	log := initLogger()
-
 	application := app.New()
-	err := application.GRPCServer.Run()
+	err := application.Run()
 	if err != nil {
-		log.Error("Failed to start gRPC server")
-		return
+		log.Fatal("Failed to start gRPC server")
 	}
 }
